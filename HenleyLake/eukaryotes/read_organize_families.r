56a269d20b0d83609e6ab7fae7a1d534b93cf9c5
Jenise Swall <jenise.swall@gmail.com>
2020-01-27 16:12:10 -0500

    Began working on reading in and organizing the eukaryote data for Henley Lake.

---
+++ HenleyLake/eukaryotes/read_organize_families.r
@@ -0,0 +1,252 @@
+library("tidyverse")
+library("readxl")
+library("stringr")
+
+
+## ##################################################
+## Read in family-level taxonomic data from Excel sheet.
+fileNm <- "orig_data_files/Henley_Lake_18S_Taxonomy_2019-12-30.xlsx"
+
+## Rows 193-197 contain summary data ("Eukaryota", "Total
+## Unclassified", etc.), so we exclude this info.  Rows 191-192 are
+## empty, and row 1 is the header.  We should have 189 rows of data.
+## Note that row 2 contains counts of all eukaryotes.
+rawAllT <- read_excel(path=fileNm, sheet="Tax Level 5-Family", n_max=189)
+rm(fileNm)
+
+## The first row contains counts for all eukaryotes, so the totals in
+## this row should equal the sums of all the rows after it.
+all.equal(as.vector(as.matrix(rawAllT[1, 6:ncol(rawAllT)])),
+          as.vector(as.matrix(apply(rawAllT[2:nrow(rawAllT), 6:ncol(rawAllT)], 2, sum))))
+## So, we remove this first row with totals of all eukaryotes.
+rawAllT <- rawAllT[-1,]
+
+## For all observations, the value of "taxlevel" is 5 (denoting
+## family-level), so we remove this column.  We also remove "rankID"
+## and "daughterlevels", since we aren't using them.
+rawAllT <- rawAllT %>% select(-taxlevel, -rankID, -daughterlevels)
+
+## The `New total` column contains the totals of all the columns following.
+all.equal(rawAllT %>% pull(`New total`), apply(rawAllT[,3:ncol(rawAllT)], 1, sum))
+
+## After these checks, we remove the "total" and "newtotal" columns.
+rawAllT <- rawAllT %>% select(-`New total`)
+## ##################################################
+
+
+
+## ##################################################
+## Concentrate on the taxa counts for the individual cadavers on the
+## various collection days.  Transfer from wide format to long format.
+
+## Go from wide to long format.
+rawIndivT <- rawAllT %>%
+  gather(sampleName, counts, -taxon)
+## ##################################################
+
+
+## ##################################################
+## Join with sampling information to get information about accumulated
+## degree days.  That tibble already has information about sample type
+## (R, S, W, or M) and collection number, so that we don't have to
+## parse it out of the taxa spreadsheet's column names.
+
+## Read in sampling information for Henley Lake, which we already
+## organized and stored in a CSV file when doing the bacterial
+## analysis.
+samplingT <- read_csv("../bacteria/sampling_info.csv")
+
+## NOTE: We also remove date, season, and collection variables.  This
+## info is contained in the sampleName variable.
+indivT <- rawIndivT %>%
+  left_join(samplingT) %>%
+  select(-date, -season, -collection)
+rm(rawIndivT, rawAllT, samplingT)
+## ##################################################
+
+
+
+## ######### WORKING HERE!!!!!
+
+
+
+
+## ##################################################
+## Consider level of classification for each taxa.  (Some could not be
+## classified down to the family level, but maybe the order could be
+## determined.)
+
+## Make new variable to indicate the most precise taxon which could be
+## identified.
+indivT$taxLvl <- ""
+indivT$taxLvl[str_detect(indivT$taxon, "fa__")] <- "family"
+indivT$taxLvl[str_detect(indivT$taxon, "o__")] <- "order"
+indivT$taxLvl[str_detect(indivT$taxon, "c__")] <- "class"
+indivT$taxLvl[str_detect(indivT$taxon, "p__")] <- "phylum"
+indivT$taxLvl[str_detect(indivT$taxon, "k__")] <- "kingdom"
+indivT$taxLvl <- ordered(indivT$taxLvl, levels=c("family", "order", "class", "phylum", "kingdom"))
+
+
+## Find percentage of counts can be classified down to the family
+## level, across all types.  We see that about 80.3% of the counts can
+## be attributed at the family level.
+indivT %>% group_by(taxLvl) %>% summarize(sumCounts=sum(counts), percCounts=100*sum(counts)/sum(indivT$counts))
+
+## Find percentage of counts which can be classified down to the
+## family level, type by type.
+indivT %>% group_by(type, taxLvl) %>% summarize(sumCountsByTypeTaxLvl=sum(counts)) %>% left_join(indivT %>% group_by(type) %>% summarize(sumCountsByType=sum(counts))) %>% mutate(perc=100*sumCountsByTypeTaxLvl/sumCountsByType)
+## Percentages classified to family level: 83.8% (ribs), 78.9%
+## (scapulae), 71.2% (water), 70.9% (mud)
+
+
+## For each sample, what percentage of counts can be classified to the
+## each level?
+percTaxLvlBySampleT <- indivT %>%
+  group_by(degdays, type, sampleName, taxLvl) %>%
+  summarize(sumTaxLvlCts=sum(counts)) %>%
+  inner_join(indivT %>% group_by(sampleName) %>%
+             summarize(sampleTotals=sum(counts))
+             ) %>%
+  mutate(percTaxLvlCts = 100*sumTaxLvlCts/sampleTotals)
+
+
+## For each sample, plot percentage classified at each level
+## (e.g. family, phylum, etc.) by type (rib, scapula, water) and ADD.
+ggplot(percTaxLvlBySampleT) +
+  geom_point(aes(x=degdays, y=percTaxLvlCts, color=taxLvl)) +
+  facet_wrap(~type, nrow=1) +
+  labs(x="Accumulated degree days", y="Percentage taxa classified each level")
+  ggsave("rr_family_perc_classif_by_add_type.pdf", width=8.5, height=4, units="in")
+## dev.off()
+## ##################################################
+
+
+
+## ##################################################
+## From this point forward, we consider only family-level taxa for our
+## model.  So, we remove all counts of non-family-level taxa.  This is
+## consistent with the anlaysis we did for Shane's and Luisa's data.
+indivT <- indivT %>% filter(taxLvl=="family")
+
+
+## Make a new, more readable taxon column.
+## Column names with open brackets (e.g. "f__[Tissierellaceae]") cause
+## problems for functions expecting traditional data frame column
+## names (like randomForest function), so we remove brackets.
+indivT$newtaxon <- gsub(indivT$taxon, pattern="\\[", replacement="")
+indivT$newtaxon <- gsub(indivT$newtaxon, pattern="]", replacement="")
+## Column names with dashes can likewise be a problem, so I replace
+## dashes with underscores.
+indivT$newtaxon <- gsub(indivT$newtaxon, pattern="-", replacement="_")
+## Remove the column of the original, less-R-friendly taxon names from
+## the tibble to avoid confusion.  Rename new column.
+indivT <- indivT %>% select(-taxon)
+indivT <- rename(indivT, taxon = newtaxon)
+
+## Count the number of unique family-level taxa observed for the
+## various types (rib, scapula, water, mud).
+indivT %>% filter(taxLvl=="family") %>% filter(counts>0) %>% group_by(type) %>% distinct(taxon) %>% summarize(n=n())
+##   type        n
+##   <chr>   <int>
+## 1 Mud       187
+## 2 Rib       236
+## 3 Scapula   264
+## 4 Water     176
+
+## For use in graphs and in calculating percentages later, we need
+## total counts for each sample.
+ctBySampleT <- indivT %>%
+  group_by(sampleName) %>%
+  summarize(totals=sum(counts))
+## ##################################################
+
+
+
+## ##################################################
+## Some taxa don't occur frequently.  It's hard to make a hard cutoff
+## for what constitutes "frequently".  There are 283 family-level taxa
+## represented, and a lot of them appear to make up less than 1% of
+## the counts for any sample.
+
+## I'm going to set the frequency cutoff at 1% (0.01).  This means
+## that in order to meet the cutoff, a specific family-level taxa must
+## make up at least 1% of the total family-level counts for that
+## sample.
+freqCutoff <- 0.01
+
+## ## Get list of maximum taxa percentages for various types (rib,
+## ## scapula, water, mud) sorted in descending order:
+## indivT %>%
+##   left_join(ctBySampleT) %>%
+##   mutate(fracBySampleName = counts/totals) %>%
+##   group_by(type, taxon) %>%
+##   summarize(maxFracBySampleName = max(fracBySampleName)) %>%
+##   ## filter(maxFracBySampleName >= freqCutoff) %>%
+##   arrange(type, desc(maxFracBySampleName)) %>%
+##   print(n = Inf)
+
+
+## Count how many times each taxa beats the cutoff (freqCutoff) within
+## each type.  Then, keep only those which meet cutoff for more than 1
+## sample.  Save those taxa names for each type.
+freqTaxaByTypeT <- indivT %>%
+  left_join(ctBySampleT) %>%
+  mutate(fracBySampleName = counts/totals,
+         isExceed=(fracBySampleName>=freqCutoff)) %>%
+  group_by(type, taxon) %>%
+  summarize(numExceed = sum(isExceed)) %>%
+  filter(numExceed > 1) %>%
+  ## arrange(type, desc(numExceed)) %>%
+  select(type, taxon)
+
+## For each type, how many taxa meet the cutoff?
+freqTaxaByTypeT %>% group_by(type) %>% summarize(n=n())
+## Save list of frequent taxa by type to a CSV file.
+write.csv(freqTaxaByTypeT, file="family_freq_taxa_by_type.csv", row.names=F)
+
+
+## Rename taxa that occur less than the frequency cutoff allows as
+## "rare".  Then, sum all these "rare" taxa into one row for each
+## sample.
+commontaxaT <- indivT
+## Find rows which contain taxa which do not meet the frequency cutoff
+## for that type.
+indicRare <- !( with(commontaxaT, paste(taxon, type, sep=":")) %in% with(freqTaxaByTypeT, paste(taxon, type, sep=":")) )
+## For these "rare" taxa, we change taxon name to rare.
+commontaxaT[indicRare, "taxon"] <- "Rare"
+## Now, we add up the rare counts so that "Rare" appears only one for
+## each sample.
+commontaxaT <- commontaxaT %>%
+  group_by(sampleName, taxon, type, degdays) %>%
+  summarize(counts = sum(counts))
+
+
+## Use the table of total counts by subj/day to find the fraction
+## represented by each taxa for each subj/day.
+commontaxaT <- commontaxaT %>%
+  left_join(ctBySampleT) %>%
+  mutate(fracBySample=counts/totals) %>%
+  select(-totals)
+
+
+## Check that the fractions add up to 1, appropriately.
+unique(
+    commontaxaT %>%
+    group_by(sampleName) %>%
+    summarize(sumFracBySample = sum(fracBySample)) %>%
+    pull(sumFracBySample)
+)
+
+
+## Remove the list of taxa names that satisfied the frequency cutoff.
+rm(indicRare, freqCutoff, freqTaxaByTypeT)
+## ##################################################
+
+
+
+## ##################################################
+## Save the tibble to a file for use in separate code
+## for graphing and analysis.
+
+write.csv(commontaxaT, file="families_massaged.csv", row.names=FALSE)
+## ##################################################
